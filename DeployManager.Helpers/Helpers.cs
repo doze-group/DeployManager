15add28717508dfd798bcd5f98ccea2b54c7ee4b
Rober19 <texas_telecaster@hotmail.com>
2018-12-15 13:12:16 -0500

    [Rober19] [12_15_2018] Badge

--- DeployManager.Helpers/Helpers.cs
+++ DeployManager.Helpers/Helpers.cs
@@ -10,12 +10,43 @@ namespace DeployManager
 {
     public static class Helpers
     {
-        const string FILE_NAME = "./Cache.data";
+        const string FILE_NAME = "./Data/Cache.data";
         //C:\Users\%user%\AppData\Local\VirtualStore\Program Files (x86)\Rober19\DeployManager.Setup
+
+        public static void c_folder()
+        {
+            // Specify the directory you want to manipulate.
+            string path = @"Data";
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("That path exists already.");
+                    return;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+
+                // Delete the directory.
+                //di.Delete();
+                //Console.WriteLine("The directory was deleted successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            finally { }
+        }
+
         public static void Serialize<T>(this List<T> obj)
         {
             using (Stream stream = File.OpenWrite(FILE_NAME))
             {
+                c_folder();
                 var formatter = new BinaryFormatter();
                 formatter.Serialize(stream, obj);
             }
